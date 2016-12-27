7c34ec0cef0d6d4bc24da06ad2f1675b650230fe
Rajan Patel <rpatel3001@gmail.com>
2016-12-27 18:55:56 -0500

    set 1 prob 4

---
+++ set1/Prob4.scala
@@ -0,0 +1,20 @@
+import Prob3.SingleByteXOR._
+
+object DetectXOR {
+	def main(args: Array[String]): Unit = {
+		var min = (' ', 1000000.0)
+		var text = ""
+		for (line <- io.Source.fromFile("data4.txt").getLines()) {
+			var tmp = decrypt(line)
+			if(tmp.length > 0) {
+				for(t <- 0 to 0) {
+					if (tmp(t)._2 < min._2) {
+						min = tmp(t)
+						text = charXOR(toASCII(line), tmp(t)._1)
+					}
+				}
+			}
+		}
+		println(min + " " + text)
+	}
+}
