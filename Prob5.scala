0b3da9fcda590df28f5ef6bbc948101a68ade066
Rajan Patel <rpatel3001@gmail.com>
2016-12-27 20:25:57 -0500

    reorg and helper

---
+++ Prob5.scala
@@ -0,0 +1,6 @@
+object Prob5 {
+	def main(args: Array[String]): Unit = {
+		var str = "Burning 'em, if you ain't quick and nimble\nI go crazy when I hear a cymbal"
+		var out = keyXOR(str, "ICE")
+	}
+}
