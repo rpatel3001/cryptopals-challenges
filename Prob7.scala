dd780fd77d128045c4d99190ea52636c49e5e4de
Rajan Patel <rpatel3001@gmail.com>
2016-12-29 16:05:01 -0500

    factor out aes decode

--- Prob7.scala
+++ Prob7.scala
@@ -5,16 +5,19 @@ import Prob3.toASCII
 
 object Prob7 {
 	def main(args: Array[String]): Unit = {
-		var key = new SecretKeySpec("YELLOW SUBMARINE".getBytes, "AES");
 		var base64 = io.Source.fromFile("data7.txt").filter(c => c != '\n').mkString
 		var bytes = Base64.getDecoder().decode(base64)
-		var cipher = Cipher.getInstance("AES/ECB/NoPadding")
-		cipher.init(Cipher.DECRYPT_MODE, key)
-		var plain = toASCII(cipher.doFinal(bytes))
-		if(plain.slice(0,33) == "I'm back and I'm ringin' the bell") {
+		if(toASCII(AESdecode(bytes, "YELLOW SUBMARINE".getBytes)).slice(0,33) == "I'm back and I'm ringin' the bell") {
 			println("Prob 7: Success")
 		} else {
 			println("Prob 7: Success")
 		}
+	}
+
+	def AESdecode(str: Array[Byte], key: Array[Byte]): Array[Byte] = {
+		var seckey = new SecretKeySpec(key, "AES");
+		var cipher = Cipher.getInstance("AES/ECB/NoPadding")
+		cipher.init(Cipher.DECRYPT_MODE, seckey)
+		cipher.doFinal(str)
 	}
 }
