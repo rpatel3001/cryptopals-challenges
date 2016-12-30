63dd9d41c2c335b9a19926ded413292823b5feaa
Rajan Patel <rpatel3001@gmail.com>
2016-12-29 20:28:04 -0500

    sbt with plugin to automate

--- Prob1.scala
+++ Prob1.scala
@@ -1,7 +1,6 @@
 import java.util.Base64
 
 object Prob1 {
-
   def main(args: Array[String]): Unit = {
     var hexstr = BigInt("49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d", 16).toByteArray
     var out = Base64.getEncoder().encodeToString(hexstr)
--- build.sbt
+++ build.sbt
@@ -1,1 +1,4 @@
+import com.typesafe.sbt.SbtStartScript
+seq(SbtStartScript.startScriptForClassesSettings: _*)
+
 scalaVersion := "2.12.1"
